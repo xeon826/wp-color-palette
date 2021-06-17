e008491bdf0eb85c99c7f46f9c4e9a49a8be54d0
asdfasdf <joshuawilkeson@timetracker365.com>
2021-06-17 17:59:20 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Settings Page WordPress Plugin
+
+![Plugin Settings Form](https://blog.wplauncher.com/wp-content/uploads/2020/07/Screen-Shot-2020-07-05-at-2.41.54-PM.png)
+
+This is a simple WordPress plugin that creates a settings page. The settings page allows WordPress admin users to add their settings for a plugin. 
+
+You can use this skeleton to add a settings page to your plugin. This plugin does the following:
+- Adds an admin menu item to the left sidebar in the WordPress admin dashboard along with a submenu item including your settings page
+- Creates a settings page that includes a form
+- Saves that form to the database
+- Makes sure the settings form fields are pre-populated if it has already been filled out
+
+Check out our [blog post](https://blog.wplauncher.com/create-wordpress-plugin-settings-page/) to get step by step instructions on [how to add a settings page to your WordPress plugin](https://blog.wplauncher.com/create-wordpress-plugin-settings-page/). The Settings Page plugin is using the [WordPress Plugin Boilerplate framework](https://github.com/DevinVinson/WordPress-Plugin-Boilerplate) and we used [WWPB.me](https://wppb.me/) to generate a version with the settings-page namespaces.
+
+## Install the Settings Page Plugin on Your WordPress Site to See What you Get
+1. Click on the green clone or download button on this page and click Download Zip
+2. Login to your WordPress site at www.your-wordpress-site.com/wp-login.php
+3. Hover over Plugins in the left sidebar
+4. Click on Add New
+5. Click on Upload and Upload the zip file that you just downloaded
+6. Activate the plugin
+
+After installation, you should see the Settings Page in the left sidebar and when you click on the Settings page, you should see a form that saves to the database and is prefilled when the page is reloaded. 
+
+
---
+++ admin/js/settings-page-admin.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function() {
+  /**
+   * All of the code for your admin-facing JavaScript source
+   * should reside in this file.
+   *
+   * Note: It has been assumed you will write jQuery code here, so the
+   * $ function reference has been prepared for usage within the scope
+   * of this function.
+   *
+   * This enables you to define handlers, for when the DOM is ready:
+   *
+   * $(function() {
+   *
+   * });
+   *
+   * When the window is loaded:
+   *
+   * $( window ).load(function() {
+   *
+   * });
+   *
+   * ...and/or other possibilities.
+   *
+   * Ideally, it is not considered best practise to attach more than a
+   * single DOM-ready or window-load handler for a particular page.
+   * Although scripts in the WordPress core, Plugins and Themes may be
+   * practising this, we should strive to set a better example in our own work.
+   */
+  const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'nano', // or 'monolith', or 'nano'
+
+    swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)'
+    ],
+
+    components: {
+
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+        hex: true,
+        rgba: true,
+        hsla: true,
+        hsva: true,
+        cmyk: true,
+        input: true,
+        clear: true,
+        save: true
+      }
+    }
+  });
+
+});
